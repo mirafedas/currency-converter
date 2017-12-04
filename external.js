de5b6c50c22f7ef937f1f79be1c675bfc5f9a1bd
Myroslava <myrosvas@gmail.com>
2017-12-05 01:07:06 +0200

    files added1

--- external.js
+++ external.js
@@ -4,3 +4,12 @@ var name = 'Max';
     event = prompt('Input an event name');
 console.log('At '+ time + ' in ' + place + ' ' + name +' will be having fun on this event: ' + event + 
 ".");
+
+
+const EU_TO_UAH = 32.1;
+const USD_TO_UAH = 27.05;
+var euro = prompt ('Input the amount in EURO');
+var dollars = prompt ('Input the amount in DOLLARS');
+alert(euro + ' EUR is/are equal to ' + euro * EU_TO_UAH + ' UAH, ' +
+dollars + ' USD is/are equal to ' + dollars * USD_TO_UAH + ' UAH, ' + 
+'one euro is equal to ' + EU_TO_UAH / USD_TO_UAH + ' USD.');
