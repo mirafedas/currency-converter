a6fef9e19a5a77817fdad43ac2813673d6e6b99c
Myroslava <myrosvas@gmail.com>
2017-12-05 00:44:12 +0200

    files added

---
+++ external.js
@@ -0,0 +1,6 @@
+var name = 'Max';
+    time = '13:25';
+    place = 'Lviv';
+    event = prompt('Input an event name');
+console.log('At '+ time + ' in ' + place + ' ' + name +' will be having fun on this event: ' + event + 
+".");
