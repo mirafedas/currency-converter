565c03022fabfc3c08dc1dd84a66c9d873af4bb5
myr <30750556+myrosvas@users.noreply.github.com>
2017-12-06 20:58:38 +0200

    Update external.js

--- external.js
+++ external.js
@@ -1,15 +1,15 @@
-var name = 'Max';
-    time = '13:25';
-    place = 'Lviv';
+var name = 'Max',
+    time = '13:25',
+    place = 'Lviv',
     event = prompt('Input an event name');
 console.log('At '+ time + ' in ' + place + ' ' + name +' will be having fun on this event: ' + event + 
 ".");
 
 
-const EU_TO_UAH = 32.1;
-const USD_TO_UAH = 27.05;
-var euro = prompt ('Input the amount in EURO');
-var dollars = prompt ('Input the amount in DOLLARS');
+var EU_TO_UAH = 32.1;
+var USD_TO_UAH = 27.05;
+var euro = prompt('Input the amount in EURO');
+var dollars = prompt('Input the amount in DOLLARS');
 alert(euro + ' EUR is/are equal to ' + euro * EU_TO_UAH + ' UAH, ' +
 dollars + ' USD is/are equal to ' + dollars * USD_TO_UAH + ' UAH, ' + 
 'one euro is equal to ' + EU_TO_UAH / USD_TO_UAH + ' USD.');
